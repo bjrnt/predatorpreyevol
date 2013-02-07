56bee4bf403f4db98e1d1241dfed1050055c99f0
Björn Tegelund <b.tegelund@gmail.com>
2013-02-07 15:05:11 +0100

    Enabled crossover and changed fitness function slightly

--- kex.py
+++ kex.py
@@ -18,7 +18,7 @@ DS.appendLinked([-1],[1])
 def calcError(x, y):
 	return abs(x - y) / x
 
-def testNetwork(eVar): # Not super good, as the best value is 0.89 and worst is 0.22
+def testNetwork(eVar):
 	global net # So that we can look at the network after we're done training
 	net = buildNetwork(1,1,1,bias=True,outputbias=True)
 	
@@ -28,20 +28,22 @@ def testNetwork(eVar): # Not super good, as the best value is 0.89 and worst is 
 	net.connections[net['bias']][1].params[0] = eVar[3];
 
 	res = net.activateOnDataset(DS)
-	fitness = 0
+	fitness = 1
 	for i in range(len(DS['target'])):
 		absError = abs(calcError(res[i], DS['target'][i]))
-		if absError < 1:
-			fitness += 1 - absError
+		if absError <= 1:
+			fitness += 1 - absError # Removes as many percent as the network is incorrect from the fitness
+		else:
+			fitness -= 1.0/len(DS) # If the network is too inaccurate it is instead punished
 
 	return fitness
 
-genome = G1DList(4) # We're only going to change 1 weight
-genome.setParams(rangemin=-10,rangemax=10) # The value should be between -1 and 1
+genome = G1DList(4) # We'll change all 4 weights in the network
+genome.setParams(rangemin=-10,rangemax=10)
 genome.initializator.set(G1DListInitializatorReal) # Initializes the genes with a Real value
 genome.mutator.set(G1DListMutatorRealGaussian) # Uses a Real value Gaussian mutator
 genome.evaluator.set(testNetwork) 
-genome.crossover.clear() # Remove the crossover function, not possible as our list has only 1 element
+#genome.crossover.clear() # Remove the crossover function, not possible if our list only has 1 element, but it seems like crossover improves the results a lot if enabled
 ga = GSimpleGA(genome)
 ga.setElitism(True) # Uses Elitism, so a few of the successful inidividuals get carried over
 ga.selector.set(GRouletteWheel) # Roulette wheel selector, see http://www.edc.ncl.ac.uk/assets/hilite_graphics/rhjan07g02.png
