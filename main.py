fcf7d81cdf74dd949d95aa076ca5b79b2a924459
Björn Tegelund <b.tegelund@gmail.com>
2013-02-26 19:28:43 +0100

    Added save and load functionality

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from creature import Creature
 from renderer import Renderer
 from bush import Bush
 from darwin import Darwin
+from disker import Disker
 #from deap import dtm
 
 import argparse, sys, cProfile, math, time
@@ -52,17 +53,21 @@ def default():
 		('World','collision'):True,
 		('World','remove_dead'):True,
 		('Creature','Bush','use_energy'):True,
-		('Creature','G_MAX_SPEED'):0.007,
-		('Darwin','NGEN'):20,
-		('Darwin','CXPB'):0.4,
+		('Creature','G_MAX_SPEED'):0.006,
+		('Darwin','NGEN'):60,
+		('Darwin','CXPB'):0.0,
 		('Darwin','MUTPB'):0.7,
 		('Darwin','NINDS'):10,
-		('Darwin','NTICKS'):400,
+		('Darwin','NTICKS'):1000,
 	}
 
 	apply_config(options)
 
 	darwin = Darwin()
+	
+	if load_file:
+		darwin.load_population(load_file)
+
 	darwin.begin_evolution()
 
 def profile():
@@ -70,10 +75,14 @@ def profile():
 
 def main():
 	parser = argparse.ArgumentParser(description='Run the simulation.')
-	parser.add_argument('-l',dest='load_path', metavar='file', type=file, help="Path to file with saved state, used to resume simulations.")
+	parser.add_argument('-l',dest='load_file', metavar='file', type=file, help="Path to file with saved state, used to resume simulations.")
 	parser.add_argument('--profile',dest='profiling_enabled', help="Use to enable or disable generation of profiling information.", required=False, action='store_const', const=True)
 	args = parser.parse_args(sys.argv[1:])
-	# args.load_path
+	global load_file
+	if args.load_file:
+		load_file = args.load_file
+	else:
+		load_file = None
 
 	if args.profiling_enabled:
 		profile()
